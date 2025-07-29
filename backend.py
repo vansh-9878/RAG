b70e2e4196bdc0dd42ccf7dd0365051aa33e7038
vansh-9878 <aroravansh0501@gmail.com>
2025-07-29 09:30:25 +0530

    added authentication

--- backend.py
+++ backend.py
@@ -1,14 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from agent.localDatabase import storeVectors
 from agent.agent import start
 from agent.localOCR import pdf_to_text
 from concurrent.futures import ThreadPoolExecutor, as_completed
-import uvicorn
+import uvicorn,os
 import requests
+from dotenv import load_dotenv
+load_dotenv()
 
 app=FastAPI()
+bearer_scheme = HTTPBearer()
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -23,13 +28,23 @@ class input(BaseModel):
     documents:str
     questions:list
 
+def verify_token(credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme)):
+    token = credentials.credentials
+    if token != os.getenv("TOKEN"):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or missing token",
+        )
+
+
+
 @app.get("/")
 def check():
     return{
         "status":"Status Running...."
     }
     
-@app.post("/")
+@app.post("/",dependencies=[Depends(verify_token)])
 def getFile(query: input):
     filePath = query.documents.split('/')[-1].split('?')[0]
     fileName = filePath.split('.')[0]
